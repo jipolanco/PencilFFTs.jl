e20976bbb60f9c91bef99e5cc1d7529ae894e3b9
Juan Ignacio Polanco <jipolanc@gmail.com>
2019-10-28 10:03:51 +0100

    Disable test_base.jl in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,18 @@
 
 import MPI: mpiexec
 
-const TEST_FILES = ("pencils.jl", "test_base.jl", "benchmarks.jl")
+const TEST_FILES = (
+    # "test_base.jl",
+    "benchmarks.jl",
+    "pencils.jl",
+)
 
 function main()
     Nproc = clamp(Sys.CPU_THREADS, 4, 8)
     julia_exec = joinpath(Sys.BINDIR, Base.julia_exename())
 
     for fname in TEST_FILES
+        @info "Running $fname with $Nproc processes..."
         run(`$mpiexec -n $Nproc $julia_exec $fname`)
     end
 
