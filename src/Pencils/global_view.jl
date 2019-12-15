f7df834e42ca874bad767ccca40385e9dbf7d410
Juan Ignacio Polanco <jipolanc@gmail.com>
2019-12-15 18:05:02 +0100

    PencilArrays are accessed with unpermuted indices
    Permutations are now performed on the fly. It's now much easier to work
    with PencilArrays regardless of their underlying index permutations.
    
    This required a custom implementation of LinearIndices() and of
    Base._sub2ind(). I'm not sure if there's a more proper way to get
    consistency between linear and Cartesian indices...

--- src/Pencils/global_view.jl
+++ src/Pencils/global_view.jl
@@ -4,14 +4,24 @@ const GlobalPencilArray{T,N} = OffsetArray{T,N,A} where {A <: PencilArray}
     global_view(x::PencilArray)
 
 Create an [`OffsetArray`](https://github.com/JuliaArrays/OffsetArrays.jl) of a
-`PencilArray` that takes global permuted indices.
+`PencilArray` that takes unpermuted global indices.
 """
 function global_view(x::PencilArray)
-    r = range_local(x, permute=true)
+    r = range_local(x, permute=false)
     offsets = first.(r) .- 1
     xo = OffsetArray(x, offsets)
     @assert parent(xo) === x  # OffsetArray shouldn't create a copy...
     xo :: GlobalPencilArray
+end
+
+# Account for index permutation in global views.
+@inline Base._sub2ind(x::GlobalPencilArray, I...) =
+    Base._sub2ind(parent(x), (I .- x.offsets)...)
+
+function Base.LinearIndices(g::GlobalPencilArray)
+    A = parent(g)
+    off = g.offsets
+    PermutedLinearIndices(LinearIndices(parent(A)), get_permutation(A), off)
 end
 
 function spatial_indices(x::GlobalPencilArray)
