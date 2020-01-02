5f8f8cdf745fafa2f21bcf142ff49a3beca45b9b
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-01-02 15:57:31 +0100

    Add --fast option to docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,8 @@
 using Documenter
 using PencilFFTs
 
+const MAKE_FAST = "--fast" in ARGS  # skip some checks in makedocs
+
 # This is to make sure that doctests in docstrings are executed correctly.
 DocMeta.setdocmeta!(PencilFFTs, :DocTestSetup,
                     :(using PencilFFTs); recursive=false)
@@ -9,32 +11,34 @@ DocMeta.setdocmeta!(PencilFFTs.Pencils, :DocTestSetup,
 DocMeta.setdocmeta!(PencilFFTs.Transforms, :DocTestSetup,
                     :(using PencilFFTs.Transforms); recursive=true)
 
-makedocs(
-    sitename = "PencilFFTs.jl",
-    format = Documenter.HTML(
-        prettyurls = true,
-        # load assets in <head>
-        assets = ["assets/custom.css",
-                  "assets/matomo.js"],
-    ),
-    modules = [PencilFFTs],
-    pages = [
-        "Home" => "index.md",
-        "tutorial.md",
-        "PencilFFTs.md",
-        "Transforms.md",
-        "Pencils.md",
-        "benchmarks.md",
-    ],
-    doctest = true,
-    # checkdocs = :exports,
-    linkcheck = true,
-)
+let with_checks = !MAKE_FAST
+    @time makedocs(
+        sitename="PencilFFTs.jl",
+        format=Documenter.HTML(
+            prettyurls=true,
+            # load assets in <head>
+            assets=["assets/custom.css",
+                    "assets/matomo.js"],
+        ),
+        modules=[PencilFFTs],
+        pages=[
+            "Home" => "index.md",
+            "tutorial.md",
+            "PencilFFTs.md",
+            "Transforms.md",
+            "Pencils.md",
+            "benchmarks.md",
+        ],
+        doctest=with_checks,
+        linkcheck=with_checks,
+        checkdocs=:all,
+    )
+end
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/jipolanco/PencilFFTs.jl",
-    forcepush = true,
+    repo="github.com/jipolanco/PencilFFTs.jl",
+    forcepush=true,
 )
