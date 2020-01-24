3a90508f93913e571cca53a06a9d891d792e9849
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-01-24 10:26:25 +0100

    Run examples in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,20 +5,30 @@
 using FFTW  # this avoids issues with precompilation of FFTW in parallel...
 import MPI: mpiexec
 
-const TEST_FILES = (
+const TEST_FILES = [
     "taylor_green.jl",
     "rfft.jl",
     "pencils.jl",
     "transforms.jl",
+]
+
+# Make sure that example files run.
+const EXAMPLE_DIR = joinpath("..", "examples")
+const EXAMPLE_FILES = joinpath.(
+    EXAMPLE_DIR,
+    filter(fname -> splitext(fname)[2] == ".jl", readdir(EXAMPLE_DIR))
 )
 
 function main()
     Nproc = clamp(Sys.CPU_THREADS, 4, 8)
     julia_exec = joinpath(Sys.BINDIR, Base.julia_exename())
 
-    for fname in TEST_FILES
+    files = [TEST_FILES..., EXAMPLE_FILES...]
+
+    for fname in files
         @info "Running $fname with $Nproc processes..."
         run(`$mpiexec -n $Nproc $julia_exec $fname`)
+        println()
     end
 
     nothing
