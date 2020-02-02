795906b9bf98828c12eb6103150bc1ed7b9e3573
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-02-02 21:19:17 +0100

    Try to support coverage in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,15 +19,27 @@ const EXAMPLE_FILES = joinpath.(
     filter(fname -> splitext(fname)[2] == ".jl", readdir(EXAMPLE_DIR))
 )
 
+# Code coverage command line options; must correspond to src/julia.h
+# and src/ui/repl.c
+# (Copied from MPI.jl)
+const JL_LOG_NONE = 0
+const JL_LOG_USER = 1
+const JL_LOG_ALL = 2
+const COVERAGE_OPTS =
+    Dict{Int, String}(JL_LOG_NONE => "none",
+                      JL_LOG_USER => "user",
+                      JL_LOG_ALL => "all")
+
 function main()
     Nproc = clamp(Sys.CPU_THREADS, 4, 8)
     julia_exec = joinpath(Sys.BINDIR, Base.julia_exename())
 
     files = [TEST_FILES..., EXAMPLE_FILES...]
+    cov = COVERAGE_OPTS[Base.JLOptions().code_coverage]
 
     for fname in files
         @info "Running $fname with $Nproc processes..."
-        run(`$mpiexec -n $Nproc $julia_exec $fname`)
+        run(`$mpiexec -n $Nproc $julia_exec --code-coverage=$cov $fname`)
         println()
     end
 
