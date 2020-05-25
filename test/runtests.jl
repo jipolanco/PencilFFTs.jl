61f6aac38c2561014607c2ecb17b991ab0d7325f
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-05-25 13:06:21 +0200

    Update runtests.jl based on MPI.jl version

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,11 @@
 # This is based on the runtests.jl file of MPI.jl.
 
 using MPI: mpiexec
+using HDF5
+using PencilFFTs
+using Test
 
-const TEST_FILES = [
+test_files = [
     "hdf5.jl",
     "taylor_green.jl",
     "rfft.jl",
@@ -13,40 +16,19 @@ const TEST_FILES = [
 ]
 
 # Make sure that example files run.
-const EXAMPLE_DIR = joinpath("..", "examples")
-const EXAMPLE_FILES = joinpath.(
-    EXAMPLE_DIR,
-    filter(fname -> splitext(fname)[2] == ".jl", readdir(EXAMPLE_DIR))
+example_dir = joinpath("..", "examples")
+example_files = joinpath.(
+    example_dir,
+    filter(fname -> splitext(fname)[2] == ".jl", readdir(example_dir))
 )
 
-# Code coverage command line options; must correspond to src/julia.h
-# and src/ui/repl.c
-# (Copied from MPI.jl)
-const JL_LOG_NONE = 0
-const JL_LOG_USER = 1
-const JL_LOG_ALL = 2
-const COVERAGE_OPTS =
-    Dict{Int, String}(JL_LOG_NONE => "none",
-                      JL_LOG_USER => "user",
-                      JL_LOG_ALL => "all")
+Nproc = clamp(Sys.CPU_THREADS, 4, 6)
+files = [test_files..., example_files...]
 
-function main()
-    Nproc = clamp(Sys.CPU_THREADS, 4, 8)
-    julia_exec = joinpath(Sys.BINDIR, Base.julia_exename())
-
-    files = [TEST_FILES..., EXAMPLE_FILES...]
-    cov = COVERAGE_OPTS[Base.JLOptions().code_coverage]
-    julia_args = ["--compiled-modules=no", "--code-coverage=$cov"]
-
-    for fname in files
-        @info "Running $fname with $Nproc processes..."
-        mpiexec() do cmd
-            run(`$cmd -n $Nproc $julia_exec $julia_args $fname`)
-        end
-        println()
+for fname in files
+    @info "Running $fname with $Nproc processes..."
+    mpiexec() do cmd
+        run(`$cmd -n $Nproc $(Base.julia_cmd()) $fname`)
     end
-
-    nothing
+    println()
 end
-
-main()
