3f4597db3f04add17e7c9cf7d6cc43aae4e1aae9
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-08-31 18:29:44 +0200

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,35 +1,29 @@
 using Documenter
-using HDF5  # to load HDF5 code via Requires
 using PencilFFTs
 
 const MAKE_FAST = Ref("--fast" in ARGS)  # skip some checks in makedocs
 
 # This is to make sure that doctests in docstrings are executed correctly.
-DocMeta.setdocmeta!(PencilFFTs, :DocTestSetup,
-                    :(using PencilFFTs); recursive=false)
-DocMeta.setdocmeta!(PencilFFTs.PencilArrays, :DocTestSetup,
-                    :(using PencilFFTs.PencilArrays); recursive=true)
-DocMeta.setdocmeta!(PencilFFTs.Permutations, :DocTestSetup,
-                    :(using PencilFFTs.Permutations); recursive=true)
 DocMeta.setdocmeta!(PencilFFTs.Transforms, :DocTestSetup,
                     :(using PencilFFTs.Transforms); recursive=true)
-DocMeta.setdocmeta!(PencilFFTs.PencilIO, :DocTestSetup,
-                    :(using PencilFFTs.PencilIO); recursive=true)
 
 function main()
     with_checks = !MAKE_FAST[]
 
     @time makedocs(
-        sitename="PencilFFTs.jl",
-        format=Documenter.HTML(
+        modules = [PencilFFTs],
+        authors = "Juan Ignacio Polanco <jipolanc@gmail.com> and contributors",
+        repo = "https://github.com/jipolanco/PencilFFTs.jl/blob/{commit}{path}#L{line}",
+        sitename = "PencilFFTs.jl",
+        format = Documenter.HTML(
             prettyurls=true,
+            canonical = "https://jipolanco.github.io/PencilFFTs.jl",
             # load assets in <head>
             assets=[
                 "assets/custom.css",
                 "assets/matomo.js",
             ],
         ),
-        modules=[PencilFFTs],
         pages=[
             "Home" => "index.md",
             "tutorial.md",
@@ -38,21 +32,10 @@ function main()
                 "examples/gradient.md",
             ],
             "Library" => [
-                "MPI-distributed arrays" => [
-                    "MPITopology.md",
-                    "Pencils.md",
-                    "PencilArrays.md",
-                    "Transpositions.md",
-                    "PencilArrays_timers.md",
-                    "Internals" => ["PermutationUtils.md"]
-                ],
-                "PencilIO.md",
-                "Distributed FFTs" => [
-                    "PencilFFTs.md",
-                    "Transforms.md",
-                    "PencilFFTs_timers.md",
-                    "Internals" => ["GlobalFFTParams.md"],
-                ]
+                "PencilFFTs.md",
+                "Transforms.md",
+                "PencilFFTs_timers.md",
+                "Internals" => ["GlobalFFTParams.md"],
             ],
             "benchmarks.md",
         ],
@@ -70,6 +53,6 @@ main()
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo="github.com/jipolanco/PencilFFTs.jl",
-    forcepush=true,
+    repo = "github.com/jipolanco/PencilFFTs.jl",
+    forcepush = true,
 )
