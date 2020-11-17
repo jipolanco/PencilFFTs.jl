b353d45086a472d0f85de192d2d7c993a2c28926
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-11-17 14:31:30 +0100

    Update benchmark results

--- benchmarks/make_sysimage.jl
+++ benchmarks/make_sysimage.jl
@@ -10,14 +10,8 @@ using PencilFFTs.PencilArrays
 import FFTW
 using MPI
 
-using ArgParse
-
 MPI.Init()
 comm = MPI.COMM_WORLD
-
-s = ArgParseSettings()
-@add_arg_table! s begin end
-parse_args(s)
 
 plan = PencilFFTPlan((32, 32, 32), Transforms.RFFT(), (1, 1), comm)
 u = allocate_input(plan)
@@ -29,9 +23,13 @@ mktemp() do fname, io
     write(io, precompile_script)
     close(io)
     @time create_sysimage(
-        [:MPI, :FFTW, :PencilFFTs, :ArgParse],
-        sysimage_path="sys_benchmarks.so",
-        cpu_target="native",
-        precompile_execution_file=fname,
+        [
+            # :MPI,
+            :FFTW,
+            :PencilFFTs,
+        ],
+        sysimage_path = "sys_benchmarks.so",
+        cpu_target = "native",
+        precompile_execution_file = fname,
     )
 end
