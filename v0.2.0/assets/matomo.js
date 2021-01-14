2cd9ccc18774bde967f27aed823aee0e9f16b833
Juan Ignacio Polanco <jipolanc@gmail.com>
2021-01-14 14:47:27 +0000

    build based on af77e3f

--- v0.2.0/assets/matomo.js
+++ v0.2.0/assets/matomo.js
@@ -1,11 +1,11 @@
-var _paq = window._paq || [];
+var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-    var u="https://bsadioqrywqelklksjd.tk/matomo/";
+    var u="https://goeiwkensasd.tk/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
+    _paq.push(['setSiteId', '3']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
