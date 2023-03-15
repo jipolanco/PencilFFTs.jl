7a87a7fa7e2334767ee2e363f42edf1fafbe69b0
matinraayai <30674652+matinraayai@users.noreply.github.com>
2023-03-15 12:40:11 -0400

    Fixed extensions.

--- ext/AMDGPUPencilFFTs.jl
+++ ext/AMDGPUPencilFFTs.jl
@@ -1,6 +1,7 @@
 module AMDGPUPencilFFTs
 
-using .Transforms
+using PencilFFTs
+using PencilFFTs.Transforms: FFT, FFT!, BFFT, BFFT!, RFFT, BRFFT
 using AMDGPU
 if AMDGPU.functional(:rocfft)
     using AMDGPU: rocFFT, AnyROCArray
@@ -8,15 +9,15 @@ end
 
 if AMDGPU.functional(:rocfft)
     # c2c.jl
-    plan(::FFT, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_fft(A, args...; kwargs...)
-    plan(::FFT!, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_fft!(A, args...; kwargs...)
-    plan(::BFFT, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_bfft(A, args...; kwargs...)
-    plan(::BFFT!, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_bfft!(A, args...; kwargs...)
+    PencilFFTs.Transforms.plan(::FFT, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_fft(A, args...; kwargs...)
+    PencilFFTs.Transforms.plan(::FFT!, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_fft!(A, args...; kwargs...)
+    PencilFFTs.Transforms.plan(::BFFT, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_bfft(A, args...; kwargs...)
+    PencilFFTs.Transforms.plan(::BFFT!, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_bfft!(A, args...; kwargs...)
 
     # r2c.jl
-    plan(::RFFT, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_rfft(A, args...; kwargs...)
+    PencilFFTs.Transforms.plan(::RFFT, A::AnyROCArray, args...; kwargs...) = rocFFT.plan_rfft(A, args...; kwargs...)
 
-    function plan(tr::BRFFT, A::AnyROCArray, dims; kwargs...)
+    function PencilFFTs.Transforms.plan(tr::BRFFT, A::AnyROCArray, dims; kwargs...)
         Nin = size(A, first(dims))  # input length along first dimension
         d = length_output(tr, Nin)
         rocFFT.plan_brfft(A, d, dims; kwargs...)
--- ext/CUDAPencilFFTs.jl
+++ ext/CUDAPencilFFTs.jl
@@ -1,21 +1,22 @@
 module CUDAPencilFFTs
 
-using .Transforms
+using PencilFFTs
+using PencilFFTs.Transforms: FFT, FFT!, BFFT, BFFT!, RFFT, BRFFT
 using CUDA
 
 # c2c.jl
 @show 212341
 
-plan(::FFT, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_fft(A, args...; kwargs...)
-plan(::FFT!, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_fft!(A, args...; kwargs...)
-plan(::BFFT, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_bfft(A, args...; kwargs...)
-plan(::BFFT!, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_bfft!(A, args...; kwargs...)
+PencilFFTs.Transforms.plan(::FFT, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_fft(A, args...; kwargs...)
+PencilFFTs.Transforms.plan(::FFT!, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_fft!(A, args...; kwargs...)
+PencilFFTs.Transforms.plan(::BFFT, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_bfft(A, args...; kwargs...)
+PencilFFTs.Transforms.plan(::BFFT!, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_bfft!(A, args...; kwargs...)
 
 # r2c.jl
 
-plan(::RFFT, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_rfft(A, args...; kwargs...)
+PencilFFTs.Transforms.plan(::RFFT, A::AnyCuArray, args...; kwargs...) = CUFFT.plan_rfft(A, args...; kwargs...)
 
-function plan(tr::BRFFT, A::AnyCuArray, dims; kwargs...)
+function PencilFFTs.Transforms.plan(tr::BRFFT, A::AnyCuArray, dims; kwargs...)
     Nin = size(A, first(dims))  # input length along first dimension
     d = length_output(tr, Nin)
     CUFFT.plan_brfft(A, d, dims; kwargs...)
